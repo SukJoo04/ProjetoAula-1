08941ae6e77aaf5a8c07efbc578f125a7eab72ab
SukJoo04 <bernardokim04@gmail.com>
2026-03-24 11:55:07 -0300

    feat:Declarações e Variáveis

---
+++ src/js/script.js
@@ -0,0 +1,46 @@
+// Declarações e Variaveis
+
+// var, let e const
+var xpto=10;
+console.log(xpto);
+var xpto=20;
+console.log(xpto);
+//let - pode redeclarar e acessar fora do escapo
+//let não pode redeclarar e não consegue acessar fora do espaco
+if(true){
+var nome= "Fiap"
+}
+console.log(nome)
+
+//const
+//const taxaJuros =0.85;
+//taxaJuros =0.50;
+//console.log(taxaJuros)
+
+var exemplo1='Ola dev'
+console.log(exemplo1);
+
+let exemplo2='Ola dev const'
+console.log(exemplo2);
+
+const exemplo3 ="ola dev const"
+console.log(exemplo3);
+
+    let exemplo4 =20;
+    console.log(exemplo4);
+
+    let exemplo5=true;
+    console.log (typeof exemplo5);
+
+    let exemplo6={nome:"Fiap"};
+    console.log (typeof exemplo6)
+    
+    let exemplo7=["Java","Python","C#"]
+    console.log(exemplo7);
+
+    //Variavel indefinida
+    let exemplo8;
+    console.log(exemplo8);
+
+    let exemplo9=null;
+    console.log(exemplo9);
