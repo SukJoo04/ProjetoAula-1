b27649a108c248273d94c46bdcbc4531d759d188
Bernardo <bernardokim04@gmail.com>
2026-04-07 10:52:27 -0300

    feat: estrutura de repetição for

--- src/js/script.js
+++ src/js/script.js
@@ -47,114 +47,123 @@ console.log(nome)
 
 // OPERADORES ARITMÉTICOS
 
-const valor1=20;
-const valor2=10;
+// const valor1=20;
+// const valor2=10;
 
-console.log(valor1 + valor2);
-console.log(valor1 / valor2);
-console.log(valor1 - valor2);
-console.log(valor1 * valor2);
+// console.log(valor1 + valor2);
+// console.log(valor1 / valor2);
+// console.log(valor1 - valor2);
+// console.log(valor1 * valor2);
 
-// OPERADORES LÓGICOS
+// // OPERADORES LÓGICOS
 
-console.log(valor1 < valor2);   // Maior
-console.log(valor1 > valor2);   // Menor
-console.log(valor1 >= valor2);  // Maior igual
-console.log(valor1 <= valor2);  // Menor igual
-console.log(valor1 = valor2);   // Atribuir
-console.log(valor1 == valor2);  // Comparar
-console.log(valor1 === valor2); // Comparar e verificar o tipo
-console.log(valor1 != valor2);  // Diferente
+// console.log(valor1 < valor2);   // Maior
+// console.log(valor1 > valor2);   // Menor
+// console.log(valor1 >= valor2);  // Maior igual
+// console.log(valor1 <= valor2);  // Menor igual
+// console.log(valor1 = valor2);   // Atribuir
+// console.log(valor1 == valor2);  // Comparar
+// console.log(valor1 === valor2); // Comparar e verificar o tipo
+// console.log(valor1 != valor2);  // Diferente
 
-console.log(valor1 < valor2);
-console.log(valor1 > 10 && valor2 != 20);
-console.log(valor2 < 5 || valor1 > 50);
+// console.log(valor1 < valor2);
+// console.log(valor1 > 10 && valor2 != 20);
+// console.log(valor2 < 5 || valor1 > 50);
 
-// OPERADORES DE COMPARAÇÃO
+// // OPERADORES DE COMPARAÇÃO
 
-console.log(valor1 < valor2);
-console.log(valor1 >= valor2);
-console.log(valor1 <= valor2);
-console.log(valor1 != valor2);
-console.log(valor1 == valor2);
-console.log(valor1 === valor2);
+// console.log(valor1 < valor2);
+// console.log(valor1 >= valor2);
+// console.log(valor1 <= valor2);
+// console.log(valor1 != valor2);
+// console.log(valor1 == valor2);
+// console.log(valor1 === valor2);
 
-console.log("O valor é: ", valor2);
+// console.log("O valor é: ", valor2);
 
-// EXERCÍCIOS
-// 1) Utilizando as variáveis p (preço do produto) e v (valor do desconto, crie uma nova variável chamada preçoFinal que subtraia o desconto do preço e exiba o resultado)
-const p = 20
-const v = 10
-const precoFinal = p - v
-console.log("O preço final com desconto é:", precoFinal)
+// // EXERCÍCIOS
+// // 1) Utilizando as variáveis p (preço do produto) e v (valor do desconto, crie uma nova variável chamada preçoFinal que subtraia o desconto do preço e exiba o resultado)
+// const p = 20
+// const v = 10
+// const precoFinal = p - v
+// console.log("O preço final com desconto é:", precoFinal)
 
-// 2) Verificação de Segurança (Login e Token) No banco, para autorizar uma transação, precisamos que o usuário esteja logado e que o token de segurança seja válido.
-const usuarioLogado = 1;
-const token = 100;
-const transacãoAutorizada = 1 && token >= 10;
-console.log("Transação Autorizada com sucesso", transaçãoAutorizada);
+// // 2) Verificação de Segurança (Login e Token) No banco, para autorizar uma transação, precisamos que o usuário esteja logado e que o token de segurança seja válido.
+// const usuarioLogado = 1;
+// const token = 100;
+// const transacãoAutorizada = 1 && token >= 10;
+// console.log("Transação Autorizada com sucesso", transaçãoAutorizada);
 
-// ESTRUTURA CONDICIONAL
+// // ESTRUTURA CONDICIONAL
 
-if(true){
-    console.log("é Verdadeiro")
+// if(true){
+//     console.log("é Verdadeiro")
+// }
+
+// // if/else
+
+// let nome="Fiap"
+
+// if(nome=="Fiap")
+// {
+//     console.log("nome Errado") 
+// }
+
+// // if encadeado ou aninhado
+
+// let idade = 17;
+// if(idade <= 13){
+//     console.log("é uma criança")
+// } else if (idade >13 && idade <=18){
+//     console.log("é um adolecente")
+// } else if (idade >18 && idade <=59)
+//     console.log("é um adulto")
+// else {
+//     console.log("é um idoso")
+// }
+
+// // SWITCH CASE
+
+// let times="santos";
+
+// switch(times){
+//     case "santos":
+//         console.log("Um time idoso")
+//         break;
+//     case "sao paulo":
+//         console.log("Time Panetone")
+//         break;
+//     case "palmeiras":
+//         console.log("O MELHOR APENAS")
+//         break
+//     case "corinthians":
+//         console.log("Time de antas")
+//         break
+//     default:
+//         console.log("é tão ruim quanto a selação brasileira")
+// }
+
+// // TERNARIA
+
+// let salario = 100;
+// let resultado = salario == 100 ? "Salario Certo":"Salario Errado"
+// console.log(resultado)
+
+// let usuario = "fiap";
+// let userLogado = usuario === "fiap"?"Logado com sucesso":"usuario inválido"
+// console.log(userLogado);
+
+// function verificarParImpar(numero){
+//     return numero % 2 === 0 ? "Par":"Impar";
+// }
+
+// console.log(verificarParImpar(10))
+
+// ESTRUTURA DE REPETIÇÃO (LAÇO DE REPETIÇÃO)
+
+// for - Estrutura de repetição utilizada quando sabemos previamente quantas vezes o código vai ser executado
+
+// declaração, operação, incremento
+for(let i=1; i<=5; i++);{
+    console.log("Numero:",i)
 }
-
-// if/else
-
-let nome="Fiap"
-
-if(nome=="Fiap")
-{
-    console.log("nome Errado") 
-}
-
-// if encadeado ou aninhado
-
-let idade = 17;
-if(idade <= 13){
-    console.log("é uma criança")
-} else if (idade >13 && idade <=18){
-    console.log("é um adolecente")
-} else if (idade >18 && idade <=59)
-    console.log("é um adulto")
-else {
-    console.log("é um idoso")
-}
-
-// SWITCH CASE
-
-let times="santos";
-
-switch(times){
-    case "santos":
-        console.log("Um time idoso")
-        break;
-    case "sao paulo":
-        console.log("Time Panetone")
-        break;
-    case "palmeiras":
-        console.log("O MELHOR APENAS")
-        break
-    case "corinthians":
-        console.log("Time de antas")
-        break
-    default:
-        console.log("é tão ruim quanto a selação brasileira")
-}
-
-// TERNARIA
-
-let salario = 100;
-let resultado = salario == 100 ? "Salario Certo":"Salario Errado"
-console.log(resultado)
-
-let usuario = "fiap";
-let userLogado = usuario === "fiap"?"Logado com sucesso":"usuario inválido"
-console.log(userLogado);
-
-function verificarParImpar(numero){
-    return numero % 2 === 0 ? "Par":"Impar";
-}
-
-console.log(verificarParImpar(10))
